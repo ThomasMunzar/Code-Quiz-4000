ab707256c3e170645a4d04aa5c3ea70a83ae81bc
Thomas Munzar <tmunzar92@gmail.com>
2023-07-04 10:21:46 -0700

    added timer, and some questions and start button

--- script.js
+++ script.js
@@ -1,19 +1,65 @@
 // GLOBAL VARIABLES 
-var score = 0;
-var timeLeft = 200;
+console.log("hello")
+var score = 0; //Use time left as score
+var timeLeft = 90;
 var question = ["what is the capital of california?", "what is the capital of colorado?"];
 var answerChoices = [["Managua", "Santiago", "Sacramento",], ["Denver", "Managua", "Oakland"]];// made of arrays
 var correctAnswer = ["Sacramento", "Denver"]; // made of strings ( an array of arrays)
 var currentQuestion = 0;
+var startButton= document.getElementById("start-btn")
+var timer = document.getElementById("timer")
 
 // HTML SELECTORS using query slector ie..
-var questionEl = document.querySelector("#question")
+//var questionEl = document.querySelector("#question")
+
+var quizQuestions=[
+    {question: "What html element makes a button?",
+     answerChoices: ["<button>","<>button<>","buttOn","BUTTON!"],
+     correctAnswer: "<button>"
+},
+{
+    question: "Question",
+    answerChoices: [""],
+    correctAnswer: "",
+},
+{
+    question: "Question",
+    answerChoices: [""],
+    correctAnswer: "",
+},
+{
+    question: "Question",
+    answerChoices: [""],
+    correctAnswer: "",
+},
+{
+    question: "Question",
+    answerChoices: [""],
+    correctAnswer: "",
+},
+{
+    question: "Question",
+    answerChoices: [""],
+    correctAnswer: "",
+}
+];
 
 
-
+var timerInterval
 // FUNCTIONS THAT WE NEED
-
+function startTimer(){
+timerInterval= setInterval(function(){
+    timeLeft --
+    timer.textContent = timeLeft
+    if (timeLeft<=0){
+        clearInterval(timerInterval)
+        //endGame()
+    }
+},1000)
+}
 function startGame() {
+    startTimer()
+    nextQuestion()
     // function that will start game/ triggered when they press START button (need event listener)
     // starts the timer: 
         // Set Interval 
@@ -24,6 +70,7 @@ function startGame() {
 }
 
 function nextQuestion(event){
+    var question= quizQuestions[currentQuestion]
     // user clicking on one of the 4 answers is an event (use event listener?)
     // triggered when user selects any answer
     // firgure out what answer the user chose
@@ -36,7 +83,7 @@ function nextQuestion(event){
     //Increment the current question by 1
     //Change the question
     // Change the choices
-    //
+    //check answer function 
 
 }
 
@@ -63,3 +110,4 @@ function saveInitials(){
 }
 
 // event listenter to trigger start function
+startButton.addEventListener("click", startGame())
